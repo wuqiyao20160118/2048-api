61e1b2891b931bd7919c19bc5f1c870c9d8052fe
Qiyao Wu <37245448+wuqiyao20160118@users.noreply.github.com>
2018-12-04 21:15:41 +0800

    Add files via upload

--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,21 @@
-from game2048.game import Game
-from game2048.displays import Display
+#from game2048.game import Game
+#from game2048.displays import Display
+from game import Game
+from displays import Display
+import numpy as np
 
 
 def single_run(size, score_to_win, AgentClass, **kwargs):
     game = Game(size, score_to_win)
+    #agent = AgentClass(game, display=Display(), **kwargs)
     agent = AgentClass(game, display=Display(), **kwargs)
-    agent.play(verbose=True)
+    #agent.play(verbose=True)
+    _ = game.reset()
+    while game.end == 0:
+        state = game.board
+        state = np.reshape(state, [1, 16]).squeeze()
+        action = agent.step()
+        game.move(action)
     return game.score
 
 
@@ -14,15 +24,12 @@ if __name__ == '__main__':
     SCORE_TO_WIN = 2048
     N_TESTS = 10
 
-    '''====================
-    Use your own agent here.'''
-    from game2048.agents import ExpectiMaxAgent as TestAgent
-    '''===================='''
+    from agents import ConvAgent
 
     scores = []
     for _ in range(N_TESTS):
         score = single_run(GAME_SIZE, SCORE_TO_WIN,
-                           AgentClass=TestAgent)
+                           AgentClass=ConvAgent)
         scores.append(score)
 
     print("Average scores: @%s times" % N_TESTS, sum(scores) / len(scores))
